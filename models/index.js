3fa30b8b4a5a1dfe3cadd59d6726b458de7c0573
sickcurse <111002422+sickcurse@users.noreply.github.com>
2024-09-29 09:46:57 -0400

    Added API routes setup in index.js to include userRoutes, postRoutes, and commentRoutes. Ensured proper routing for users, posts, and comments with organized middleware structure.

---
+++ models/index.js
@@ -0,0 +1,26 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+// A Post can have many Comments
+Post.hasMany(Comment, {
+  foreignKey: 'post_id',
+  onDelete: 'CASCADE',
+});
+
+// A Comment belongs to a single Post
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id',
+});
+
+// A Comment belongs to a User
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+// A User can have many Comments
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+});
+
+module.exports = { User, Post, Comment };
