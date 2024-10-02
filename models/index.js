4b63174670a2e41eb28b6686e70e57afa582dd74
sickcurse <111002422+sickcurse@users.noreply.github.com>
2024-10-01 23:24:36 -0400

    fixed index.js modolue export

--- models/index.js
+++ models/index.js
@@ -2,23 +2,31 @@ const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment');
 
+// Define associations
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+});
+
 Post.belongsTo(User, {
-  foreignKey: 'userId',
+  foreignKey: 'user_id',
   onDelete: 'CASCADE',
 });
 
 Post.hasMany(Comment, {
-  foreignKey: 'postId',
+  foreignKey: 'post_id',
+  onDelete: 'CASCADE',
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id',
   onDelete: 'CASCADE',
 });
 
 Comment.belongsTo(User, {
-  foreignKey: 'userId',
-  onDelete: 'CASCADE',
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL',
 });
 
-module.exports = {
-  User,
-  Post,
-  Comment,
-};
+// Export models
+module.exports = { User, Post, Comment };
